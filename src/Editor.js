a362c07e542b1c4a496155f0f5cd79c22890c52f
HanJiyao <hanjiyao0@gmail.com>
2018-06-06 20:17:07 +0000

    del cdn

--- src/Editor.js
+++ src/Editor.js
@@ -5,12 +5,13 @@ import {
     ContentState
 } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
-import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 import Textarea from "react-textarea-autosize";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
 const style = {
     editorWrapper:{
         maxWidth: '900px',
@@ -186,10 +187,6 @@ class PostEditor extends Component {
     render() {
         return (
             <div style={style.editorWrapper}>
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
-                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
                 <div style={style.editorHeader} className="container">
                     <div className="row">
                         <div className="col-sm-3 input-group">
